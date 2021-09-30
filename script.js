9a340ca360925231d887c400af874d5fc4767fb8
Abhishek <76660005+abhishek213-alb@users.noreply.github.com>
2021-10-01 03:54:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "11 September 2021";
+const newYears = "1 May 2022";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
